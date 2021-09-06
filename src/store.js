a605b7c1da2a44779c90cbe83550acf943a5499b
Sameep <sameepsawant10@gmail.com>
2021-09-06 10:36:00 +0530

    redux fixed

--- src/store.js
+++ src/store.js
@@ -9,7 +9,10 @@ const store = createStore(
   rootreducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? (a) => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
