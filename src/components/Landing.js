4d707a2719dc3bb1cf8b79fb55036178bd3c71f3
Sameep <sameepsawant10@gmail.com>
2021-09-29 02:16:07 +0530

    feat: logo added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
+import Logo from "../images/RFWT.png";
 function Landing() {
   const history = useHistory();
   useEffect(() => {
@@ -10,23 +11,15 @@ function Landing() {
   });
   return (
     <div className="h-viewHeight bg-sidebarBackgroundColor ">
+      <div className="w-full">
+        <img src={Logo} alt="Logo" className="block mx-auto pt-20" />
+      </div>
       {/* Header  */}
       <div className="text-center pt-10 text-white">
         <h1 className="text-xl lg:text-3xl">Remote First Work Tracker</h1>
       </div>
       {/* Information about the app */}
-      <div className="text-center pt-10 lg:pt-16 w-full lg:px-64">
-        <p className=" text-sm sm:text-sm md:text-sm lg:text-4xl text-gray-400">
-          Track your daily,weekly and monthly work with - RFWT(Remote first work
-          tracker)
-        </p>
-        <p className="text-sm text-gray-300 pt-16 lg:px-24 px-5">
-          RFWT help you to track your work which you are doing or done
-          previously. You can also share the work with the team to get help from
-          the team if required any now with <strong>Loom</strong> support, which
-          allow user to record video and send it as message.
-        </p>
-      </div>
+
       {/* Login and Sign in */}
       <div className="flex justify-center pt-16">
         <Link to="/login">
